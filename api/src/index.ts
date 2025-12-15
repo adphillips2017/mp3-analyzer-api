cc5455b99eb269ed23b256b86f74014caf8acd6d
Adam Phillips <adphillips2017@users.noreply.github.com>
2025-12-15 13:03:18 -0500

    Forwarding /file-upload to /api/file-upload

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,11 +3,17 @@ import routes from './routes';
 import { setupMiddleware } from './middleware';
 import { ROUTES } from './constants/Routes';
 import { PORT } from './constants/Server';
+import analyzeRouter from './routes/analyze';
 
 const app = express();
 
 // Middleware
 setupMiddleware(app);
+
+// Forward /file-upload to /api/file-upload
+// additional measure to ensure app meets the requirement:
+// "application must host an endpoint at /file-upload"
+app.use(ROUTES.FILE_UPLOAD, analyzeRouter);
 
 // Routes - mount all routes under /api
 app.use(ROUTES.API_BASE, routes);
